0550a1f7dbcb09f7a41a7711285a6639f8d579fa
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-08-24 17:19:48 +0330

    added structure

--- app.vue
+++ app.vue
@@ -1,12 +1,5 @@
 <template>
-<<<<<<< HEAD
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-=======
-    <h1 class="">
-        kir to mansour
-        <Button test="asdad"></Button>
-    </h1>
->>>>>>> cddee362424bfb6cd53d93e93cdb28a65390920f
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-<<<<<<< HEAD
     devtools: { enabled: false },
     css: ["~/assets/css/tailwind.css"],
 
@@ -13,17 +12,3 @@ export default defineNuxtConfig({
 
     compatibilityDate: "2024-08-24",
 });
-=======
- devtools: { enabled: false },
- css: ['~/assets/css/tailwind.css'],
-
- postcss: {
-					plugins: {
-									tailwindcss: {},
-									autoprefixer: {},
-					},
-	},
-
- compatibilityDate: '2024-08-24',
-})
->>>>>>> cddee362424bfb6cd53d93e93cdb28a65390920f
