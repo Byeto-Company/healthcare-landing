22756f74bbab54bed0214ca0639de49658857e45
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-08-28 20:35:19 +0330

    added fontawesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: false },
+
+    app: {
+        head: {
+            link: [
+                {
+                    rel: "stylesheet",
+                    type: "text/css",
+                    href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+                }
+            ]
+        }
+    },
+
     css: [
         "~/assets/css/tailwind.css",
         "~/assets/css/style.css"
