7b1008a8f64b1e54ea2fc18ff86a5e21d89fe9d2
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-08-28 19:51:28 +0330

    added footer

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -3,9 +3,17 @@
 </script>
 
 <template>
-    <div>
 
+    <div class="sticky top-0 z-50 w-full shadow-sm [background-image:_url('/pattern-hexagone.png')] [background-size:_100%] bg-gray-950">
+        <div class="flex-col-center min-h-[25rem] container h-[4.75rem] padding-main">
+
+            <div class="bg-primary px-[3.5rem] py-[3rem] flex items-center justify-between [background-image:_url('/pattern-curve.png')] [background-size:_100%] rounded-250">
+
+            </div>
+
+        </div>
     </div>
+    
 </template>
 
 <style scoped>
