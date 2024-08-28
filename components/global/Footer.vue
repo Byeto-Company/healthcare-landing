89739a9d0e2f46c01d4499f800a81829515b0df0
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-08-28 20:33:17 +0330

    added footer links

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -5,10 +5,90 @@
 <template>
 
     <div class="sticky top-0 z-50 w-full shadow-sm [background-image:_url('/pattern-hexagone.png')] [background-size:_100%] bg-gray-950">
-        <div class="flex-col-center min-h-[25rem] container h-[4.75rem] padding-main">
+        <div class="container w-full h-full gap-12 flex-col-center padding-main">
 
-            <div class="bg-primary px-[3.5rem] py-[3rem] flex items-center justify-between [background-image:_url('/pattern-curve.png')] [background-size:_100%] rounded-250">
+            <div class="bg-primary w-[90%] px-[3.5rem] py-[3rem] flex items-center justify-between [background-image:_url('/pattern-curve.png')] [background-size:_100%] rounded-250">
 
+                <div class="flex flex-col w-1/2 items-start gap-[2rem]">
+
+                    <span class="text-4xl text-white">با ما در ارتباط باشید...</span>
+
+                    <p class="text-white">در این قسمت با حوزه های فعالیت شرکت آشنا میشوید و شناختی کامل از تمامی خدمات شرکت و تیم قدرتمند ما پیدا میکنید</p>
+
+                </div>
+
+                <div class="flex flex-col items-end gap-[1rem]">
+
+                    <button class="py-2.5 text-white px-7 bg-white/20 rounded-360">
+                        درخواست همکاری
+                    </button>
+
+                    <button class="py-2.5 bg-white px-7 text-primary rounded-360">
+                        ارتباط با ما
+                    </button>
+
+                </div>
+
+            </div>
+
+            <div class="flex-center w-[90%] pb-12 border-b border-gray-800">
+
+                <div class="flex flex-col gap-[2rem] w-1/2">
+
+                    <img src="/logo.png" class="w-[10rem]" alt="logo">
+                    <div class="flex flex-col gap-2">
+                        <p class="text-sm text-white">
+                            شرکت فنی مهندسی خورشید رایانه طلوع
+                        </p>
+                        <p class="text-sm text-white">
+                            آدرس : شیراز خیابان میزرای شیرازی کوچه 16 میلاد - پلاک 129
+                        </p>
+                    </div>
+                    <div class="flex items-center gap-8">
+
+                        <NuxtLink class="text-white">
+                            درباره ما
+                        </NuxtLink>
+                        <NuxtLink class="text-white">
+                            حوزه کاری
+                        </NuxtLink>
+                        <NuxtLink class="text-white">
+                            محصولات
+                        </NuxtLink>
+                        <NuxtLink class="text-white">
+                            ویژگی ها
+                        </NuxtLink>
+
+                    </div>
+
+                </div>
+
+                <div class="flex items-start justify-end w-1/2 gap-16">
+                    
+                    <div class="flex flex-col items-start gap-[1rem]">
+                        <span class="text-primary">ایمیل</span>
+                        <span class="text-white">ar.shekoohi@gmail.com</span>
+                        <span class="text-white">info@sunrisesys.ir</span>
+                    </div>
+
+                    <div class="flex flex-col items-start gap-[1rem]">
+                        <span class="text-primary">تلفن های تماس</span>
+                        <span style="direction: ltr !important;" class="text-white">+98 921 1997621</span>
+                        <span style="direction: ltr !important;" class="text-white">+98 917 8338212</span>
+                        <span style="direction: ltr !important;" class="text-white">+98 921 1997621</span>
+                        <span style="direction: ltr !important;" class="text-white">+98 917 8338212</span>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="w-[90%] flex justify-between items-center py-4">
+                <p class="text-gray-500">@2024 شرکت فنی مهندسی خورشید رایانه طلوع</p>
+
+                <div class="flex items-center gap-5">
+                    
+                </div>
             </div>
 
         </div>
