6a0d14719713fffbbe23a6b579f261474bca7555
marzban-dev <marzban.dev@gmail.com>
2024-08-29 22:52:26 +0330

    Add leaders component

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,8 @@
 <template>
     <div class="paddin-main w-full h-full flex flex-col gap-[6.25rem]">
         <Hero />
-         <WorkCategories />
+        <WorkCategories />
+        <Leaders />
     </div>
 </template>
 
