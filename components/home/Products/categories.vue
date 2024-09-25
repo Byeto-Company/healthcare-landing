96588a1640f0c1a17e0ea03d9dcf202639a60f66
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-09-25 03:57:02 +0330

    responsive categories in product slider

--- components/home/Products/categories.vue
+++ components/home/Products/categories.vue
@@ -35,32 +35,36 @@ const categories = computed(() => {
 </script>
 
 <template>
-    <div class="w-full flex justify-center items-center">
+    <div class="flex items-center justify-center w-full">
         <div
-            class="bg-gray-700/70 flex justify-center items-center gap-[100px] mt-16 mb-10 w-full max-w-[1200px] px-[100px] rounded-100 ">
-            <button
-                class="text-white text-xl relative h-full py-4"
-                @click="emit('changeActiveCategory','all')"
-                :class="activeCategory === 'all' ? 'font-bold text-primary' : ''"
-            >
-                <span :class="{'bg-gradient-to-r from-primary to-secondary text-gradient' : activeCategory === 'all'}">همه</span>
-                <span v-if="activeCategory === 'all'"
-                      class="absolute bottom-0 block w-full h-1 rounded-360 bg-gradient-to-r from-primary to-secondary">
-                </span>
-            </button>
-            <button
-                class="text-white text-xl relative h-full py-4"
-                v-for="category in categories"
-                @click="emit('changeActiveCategory',category)"
-                :class="activeCategory === category ? 'font-bold text-primary' : ''"
-            >
-                <span
-                    :class="{'bg-gradient-to-r from-primary to-secondary text-gradient' : activeCategory === category}">{{ category
-                    }}</span>
-                <span v-if="activeCategory === category"
-                      class="absolute bottom-0 block w-full h-1 rounded-360 bg-gradient-to-r from-primary to-secondary">
-                </span>
-            </button>
+            class="w-full mt-16 mb-10 overflow-x-auto bg-gray-700/70 flex-center rounded-100"
+        >
+            <div class="w-max flex-center ps-80 lg:gap-[6.25rem] gap-10 lg:px-[6.25rem]">
+                <button
+                    class="relative h-full py-4 w-[4rem] text-xl text-white"
+                    @click="emit('changeActiveCategory','all')"
+                    :class="activeCategory === 'all' ? 'font-bold text-primary' : ''"
+                >
+                    <span :class="{'bg-gradient-to-r from-primary to-secondary text-gradient' : activeCategory === 'all'}">همه</span>
+                    <span v-if="activeCategory === 'all'"
+                        class="absolute bottom-0 block w-full h-1 rounded-360 bg-gradient-to-r from-primary to-secondary">
+                    </span>
+                </button>
+                <button
+                    class="relative h-full py-4 min-w-[4rem] text-xl text-white"
+                    v-for="(category, index) in categories"
+                    :key="index"
+                    @click="emit('changeActiveCategory',category)"
+                    :class="activeCategory === category ? 'font-bold text-primary' : ''"
+                >
+                    <span
+                        :class="{'bg-gradient-to-r from-primary to-secondary text-gradient' : activeCategory === category}">{{ category
+                        }}</span>
+                    <span v-if="activeCategory === category"
+                        class="absolute bottom-0 block w-full h-1 rounded-360 bg-gradient-to-r from-primary to-secondary">
+                    </span>
+                </button>
+            </div>
         </div>
     </div>
 </template>
