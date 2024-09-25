b90ff967eb2e7a4b21060b9d7117dff631826c71
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-09-25 03:56:20 +0330

    responsive product grid

--- components/home/Products/productsGrid.vue
+++ components/home/Products/productsGrid.vue
@@ -3,7 +3,7 @@
 // types
 
 type Props = {
-    activeProduct: number;
+    activeProduct: Product;
     products: Product[];
 }
 
@@ -35,25 +35,25 @@ defineProps<Props>();
 </script>
 
 <template>
-    <div class="w-full flex justify-center items-center">
-        <div class="w-full grid grid-cols-3 gap-4 justify-items-center max-w-[1200px] justify-center items-center">
+    <div class="flex items-center justify-center w-full">
+        <div class="w-full grid grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-4 justify-items-center max-w-[1200px] justify-center items-center">
             <button
                 @click="openProduct(product)"
-                class="px-6 py-4 rounded-150 flex items-center gap-8 border-2 active:scale-95 bg-gray-600/20 transition-all border-transparent w-full justify-center"
+                class="flex items-center justify-center w-full gap-8 px-6 py-4 transition-all border-2 border-transparent rounded-150 active:scale-95 bg-gray-600/20"
                 :class="{'bg-gradient-to-r from-primary/10 to-secondary/10 !border-secondary' : product.id === activeProduct.id}"
                 v-for="(product, index) in products"
                 :key="index"
             >
                 <span
                     :class="{'text-gradient bg-gradient-to-r from-primary to-secondary' : product.id === activeProduct.id}"
-                    class="text-xl text-white font-bold transition-all"
+                    class="text-xl font-bold text-white transition-all"
                 >
                         {{ product.title }}
                 </span>
                 <div
                     class="rounded-360 overflow-hidden size-[60px] relative"
                 >
-                    <img :src="product.image" :alt="product.title" class="object-cover absolute w-full h-full" />
+                    <img :src="product.image" :alt="product.title" class="absolute object-cover w-full h-full" />
                 </div>
             </button>
         </div>
