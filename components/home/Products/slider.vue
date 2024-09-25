c8283808730748e23279036cee53a6d965deda10
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-09-25 03:57:28 +0330

    responsive products slider

--- components/home/Products/slider.vue
+++ components/home/Products/slider.vue
@@ -7,7 +7,7 @@ import { Swiper, SwiperSlide } from "swiper/vue";
 // types
 
 type Props = {
-    activeProduct: number;
+    activeProduct: Product;
     products: Product[];
 }
 
@@ -39,12 +39,12 @@ watch(
 
 <template>
     <div id="slider" class="flex flex-col items-center justify-center gap-8 mt-16">
-        <div class="flex items-center w-2/3 gap-10">
+        <div class="flex items-center w-full gap-10 lg:w-2/3">
             <button
                 :disabled="swiperInstance?.isBeginning"
                 :class="{'!grayscale' : swiperInstance?.isBeginning}"
                 @click="swiperInstance?.slidePrev()"
-                class="flex justify-center items-center shrink-0 size-[65px] rounded-360 bg-primary transition-all click-effect hover-effect"
+                class="lg:flex justify-center hidden items-center shrink-0 size-10 lg:size-[4rem] rounded-360 bg-primary transition-all click-effect hover-effect"
             >
                 <i class="fa-solid fa-chevron-right text-[24px] text-white"></i>
             </button>
@@ -54,12 +54,11 @@ watch(
                 :space-between="50"
                 centered-slides
                 @swiper="onSwiper"
-                @slide-change="onSlideChange"
                 class="w-full overflow-hidden rounded-150"
             >
                 <SwiperSlide v-for="slide in activeProduct?.slides" :key="slide.id">
                     <div
-                        class="relative flex items-center justify-center w-full overflow-hidden aspect-video rounded-150">
+                        class="relative flex items-center justify-center w-full overflow-hidden aspect-video rounded-100 lg:rounded-150">
                         <img class="absolute object-cover w-full h-full" :src="slide.image" :alt="slide.description" />
                     </div>
                 </SwiperSlide>
@@ -68,7 +67,7 @@ watch(
                 :disabled="swiperInstance?.isEnd"
                 :class="{'!grayscale' : swiperInstance?.isEnd}"
                 @click="swiperInstance?.slideNext()"
-                class="flex justify-center items-center shrink-0 size-[65px] rounded-360 bg-primary transition-all click-effect hover-effect"
+                class="lg:flex justify-center hidden items-center shrink-0 size-10 lg:size-[4rem] rounded-360 bg-primary transition-all click-effect hover-effect"
             >
                 <i class="fa-solid fa-chevron-left text-[24px] text-white"></i>
             </button>
@@ -78,9 +77,28 @@ watch(
                 {{ swiperInstance && activeProduct!.slides[swiperInstance.activeIndex].description }}
             </span>
         </div>
+        <div class="flex items-center justify-center w-full gap-5 lg:hidden">
+            <button
+                :disabled="swiperInstance?.isBeginning"
+                :class="{'!grayscale' : swiperInstance?.isBeginning}"
+                @click="swiperInstance?.slidePrev()"
+                class="flex justify-center items-center shrink-0 size-10 lg:size-[4rem] rounded-360 bg-primary transition-all click-effect hover-effect"
+            >
+                <i class="fa-solid fa-chevron-right lg:text-[2.5rem] text-white"></i>
+            </button>
+            <button
+                :disabled="swiperInstance?.isEnd"
+                :class="{'!grayscale' : swiperInstance?.isEnd}"
+                @click="swiperInstance?.slideNext()"
+                class="flex justify-center items-center shrink-0 size-10 lg:size-[4rem] rounded-360 bg-primary transition-all click-effect hover-effect"
+            >
+                <i class="fa-solid fa-chevron-left lg:text-[2.5rem] text-white"></i>
+            </button>
+        </div>
         <div class="flex items-center justify-center gap-4">
             <button
                 v-for="(_slide, index) in swiperInstance?.slides"
+                :key="index"
                 @click="swiperInstance?.slideTo(index)"
                 class="bg-gray-800 size-[12px] rounded-360 transition-all"
                 :class="{'!bg-primary' : swiperInstance?.activeIndex === index}"
