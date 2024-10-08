bbf3bd53920305d4ddf7ac103d64c0523fdeb9d1
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-08 23:14:30 +0330

    updated axios config

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
-import { useRouter } from "vue-router";
 
 export default defineNuxtPlugin(() => {
 
     const config = useRuntimeConfig()
-    const router = useRouter()
 
     const axiosInstance = axios.create({
         baseURL: config.public.API_BASE_URL,
@@ -15,76 +13,7 @@ export default defineNuxtPlugin(() => {
         }
     });
 
-    axiosInstance.interceptors.response.use(
-        response => {
-
-            const { data } = response
-
-            if ('message' in data) {
-                if (data.status >= 200 && data.status <= 210) {
-                    toast.success(data.message)
-                } else if (data.status >= 400 && data.status <= 500) {
-                    toast.error(data.message)
-                }
-            }
-
-            return Promise.resolve(response)
-        },
-        error => {
-
-            if (error instanceof axios.Cancel) {
-                return Promise.reject(error)
-            }
-
-            if ('response' in error && 'message' in error.response['data']) {
-                toast.error(error.response['data'].message)
-            }
-
-            switch (error.code) {
-                case 'ERR_NETWORK':
-                    toast.error('خطای اتصال :(')
-                    return Promise.reject({
-                        response: {
-                            data: {
-                                error: error.message
-                            }
-                        },
-                    })
-                case 'ECONNABORTED':
-                    return Promise.reject({
-                        response: {
-                            data: {
-                                code: error.code,
-                                error: error.message
-                            }
-                        }
-                    })
-            }
-
-            const { status } = error.response
-
-            // Show the user a 500 error
-            if (status >= 500) {
-                return Promise.reject({
-                    response: {
-                        data: {
-                            code: error.code,
-                            error: error.message
-                        }
-                    }
-                })
-            }
-
-            if (status === 401) {
-                toast.error('لطفا ابتدا وارد حساب کاربری خود شوید')
-                setToken('')
-                router.replace('/login')
-            }
-
-
-            return Promise.reject(error)
-        }
-    )
+    axiosInstance.interceptors.response.use()
 
     return {
         provide: {
