7780bfcfe924e4ae6480179680ae6c854a05c20f
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-08 23:11:36 +0330

    updated hero

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,19 +1,57 @@
 <script setup lang="ts">
 
+// types
+
+    type Props = {
+        heroData: HeroProps,
+        logo: {
+            link: string,
+            alt: string
+        }
+    };
+
+// props
+
+    const props = defineProps<Props>()
+
+    const { heroData, logo } = toRefs(props)
+
+// state
+
+    const config = useRuntimeConfig()
+
+// computed
+
+    const heroImages: {link: string, alt: string}[] = computed(() => {
+        return heroData.value.images.map((item) => {
+            return {
+                link: `${config.public.API_BASE_URL}/${item.link}`,
+                alt: item.alt
+            }
+        })
+    })
+
+    const heroLogo: {link: string, alt: string} = computed(() => {
+        return {
+            link: `${config.public.API_BASE_URL}/${logo.value.link}`,
+            alt: logo.value.alt
+        }
+    })
+
 </script>
 
 <template>
     <header class="relative container h-full padding-main flex-center max-md:flex-col-reverse w-full md:gap-10 xl:gap-16 lg:w-[90%] pt-8">
 
-        <div class="flex flex-col items-center lg:items-start w-full md:w-1/2 gap-4 xl:gap-8">
+        <div class="flex flex-col items-center w-full gap-4 lg:items-start md:w-1/2 xl:gap-8">
             <div class="flex flex-col items-center lg:items-start">
                 <span class="py-0.5 text-3xl xl:text-5xl !leading-[150%] font-bold">شرکت مهندسی</span>
-                <span class="py-0.5 text-3xl xl:text-5xl !leading-[150%] font-bold text-gradient bg-gradient-to-r from-primary to-secondary">خورشید رایان طلوع</span>
+                <span class="py-0.5 text-3xl xl:text-5xl !leading-[150%] font-bold text-gradient bg-gradient-to-r from-primary to-secondary">{{ heroData.title }}</span>
             </div>
             <p class="xl:text-xl text-center lg:text-start !leading-[175%] text-gray-800">
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است.
+                {{ heroData.description }}
             </p>
-            <div class="flex items-center justify-center lg:justify-start gap-3 lg:gap-5 max-xl:mt-3">
+            <div class="flex items-center justify-center gap-3 lg:justify-start lg:gap-5 max-xl:mt-3">
 
                 <NuxtLink>
                     <button class="whitespace-nowrap text-sm lg:text-[1rem] bg-gradient-to-r from-primary to-secondary px-8 xl:px-10 py-1.5 xl:py-2.5 rounded-360 text-white">
@@ -30,34 +68,34 @@
             </div>
         </div>
 
-        <div class="flex items-center justify-center md:justify-end w-full md:w-1/2">
+        <div class="flex items-center justify-center w-full md:justify-end md:w-1/2">
 
             <div class="relative flex-center aspect-square size-[430px]">
 
                 <!-- top shapes -->
 
                 <div class="w-[6rem] xl:w-[10rem] h-[10rem] xl:h-[14rem] translate-x-[100%] translate-y-[-38%] absolute overflow-hidden rounded-tl-[40px] rounded-bl-[40px] rounded-br-[40px] rounded-tr-[160px]">
-                    <img src="/shape-1.png" class="object-fill size-full">
+                    <NuxtImg :src="heroImages[0].link" :alt="heroImages[0].alt" class="object-fill size-full" />
                 </div>
 
                 <div class="w-[10rem] xl:w-[14rem] h-[10rem] xl:h-[14rem] translate-x-[-40%] translate-y-[-65%] absolute overflow-hidden rounded-tl-[160px] rounded-bl-[40px] rounded-br-[160px] rounded-tr-[160px]">
-                    <img src="/shape-1.png" class="object-fill size-full">
+                    <NuxtImg :src="heroImages[1].link" :alt="heroImages[1].alt" class="object-fill size-full" />
                 </div>
 
                 <!-- logo -->
 
                 <div class="p-4 bg-white flex-center rounded-360 xl:mt-6">
-                    <img src="/logo.png" class="size-7 xl:size-10" alt="logo">
+                    <NuxtImg :src="heroLogo.link" :alt="heroLogo.alt" class="size-7 xl:size-10" />
                 </div>
 
                 <!-- bottom shapes -->
 
                 <div class="w-[6rem] xl:w-[10rem] h-[10rem] xl:h-[14rem] translate-x-[-100%] translate-y-[50%] absolute overflow-hidden rounded-tl-[40px] rounded-bl-[160px] rounded-br-[40px] rounded-tr-[40px]">
-                    <img src="/shape-1.png" class="object-fill size-full">
+                    <NuxtImg :src="heroImages[2].link" :alt="heroImages[2].alt" class="object-fill size-full" />
                 </div>
 
                 <div class="w-[10rem] xl:w-[14rem] h-[6rem] xl:h-[10rem] translate-x-[40%] translate-y-[100%] absolute overflow-hidden rounded-tl-[40px] rounded-bl-[160px] rounded-br-[40px] rounded-tr-[40px]">
-                    <img src="/shape-1.png" class="object-fill size-full">
+                    <NuxtImg :src="heroImages[3].link" :alt="heroImages[3].alt" class="object-fill size-full" />
                 </div>
 
             </div>
