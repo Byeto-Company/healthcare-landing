34cee8461269b32fe6509a74725965fafeb17a50
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-08 23:14:22 +0330

    updated index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// imports
+
+    import useGetHomepage from "~/composables/api/homepage/useGetHomePage";
+    import { QUERY_KEYS } from "~/constants";
+
+// state
+
+    const { $queryClient: queryClient } = useNuxtApp();
+
+// queries
+
+    const { data: homepageData } = useGetHomepage()
+
+    onServerPrefetch(async () => {
+        await queryClient.prefetchQuery({
+            queryKey: [QUERY_KEYS.homepage]
+        })
+    })
+
+</script>
 
 <template>
-    <div class="w-full h-full flex flex-col items-center gap-[6.25rem]">
-        <Hero />
-        <WorkCategories />
-        <Products />
+    <div class="w-full h-full flex flex-col items-center gap-[6.25rem]" v-if="homepageData">
+        <Hero :heroData="homepageData.hero" :logo="homepageData.logo" />
+        <WorkFields :workFieldsData="homepageData.work_fields" />
+        <Products :productsData="homepageData.products" />
         <Leaders />
         <Confirmations />
         <StateMap />
