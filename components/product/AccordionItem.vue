96f1425865c8b8fad56cf0fc706d526748b0efb3
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-10 06:49:51 +0330

    changed categories accordion component

--- components/product/AccordionItem.vue
+++ components/product/AccordionItem.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
-
 // types
 
 type Props = {
-    title: string,
-    index: number,
-    id: string
-    count: number,
-    currentAccordionId: string | null,
-    items: [
-        {
-            title: string,
-            path: string
-        }
-    ]
-}
+    title: string;
+    index: number;
+    id: string;
+    currentAccordionId: string | null;
+    items: {
+        id: number;
+        name: string;
+        product_icon_photo: string;
+        slug: string;
+    }[];
+};
 
 // emit
 
@@ -26,10 +24,13 @@ const props = defineProps<Props>();
 
 const { currentAccordionId, id } = toRefs(props);
 
+// state
+
+const route = useRoute();
+
 // computed
 
 const isActive = computed(() => currentAccordionId.value == id.value);
-
 </script>
 
 <template>
@@ -41,23 +42,32 @@ const isActive = computed(() => currentAccordionId.value == id.value);
             class="flex items-center justify-between w-full gap-3 py-4 font-medium text-gray-500"
         >
             <span>{{ title }}</span>
-            <i class="text-sm transition-all fa-solid fa-chevron-down" :class="isActive ? 'rotate-180' : ''"></i>
+            <i
+                class="text-sm transition-all fa-solid fa-chevron-down"
+                :class="isActive ? 'rotate-180' : ''"
+            ></i>
         </button>
-        <div :class="isActive ? 'pb-4 grid-rows-[1fr]' : 'grid-rows-[0fr]'"
-             class="grid overflow-hidden transition-all duration-300 ease-in-out gap-4">
-            <div class="overflow-hidden">
+        <div
+            :class="isActive ? 'pb-4 grid-rows-[1fr]' : 'grid-rows-[0fr]'"
+            class="grid gap-4 overflow-hidden transition-all duration-300 ease-in-out"
+        >
+            <div class="flex flex-col gap-3 overflow-hidden">
                 <NuxtLink
                     v-for="(link, index) in items"
                     :key="index"
-                    :to="link.path"
+                    :to="{ name: 'product-slug', params: { slug: link.slug } }"
+                    class="text-sm font-medium"
+                    :class="
+                        route.params['slug'] == link.slug
+                            ? 'text-gradient bg-clip-text bg-gradient-to-r from-secondary to-primary'
+                            : ''
+                    "
                 >
-                    {{ link.title }}
+                    {{ link.name }}
                 </NuxtLink>
             </div>
         </div>
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
