3c1e57e36d26b54ab41658f2e06fd9a99b2bc752
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-10 06:49:18 +0330

    changed validation logic for formInput component

--- components/global/FormInput.vue
+++ components/global/FormInput.vue
@@ -1,52 +1,49 @@
 <script setup lang="ts">
-
 // emit
 
-    defineEmits(['update:modelValue'])
+defineEmits(["update:modelValue"]);
 
 // types
 
-    type Props = { 
-        label: string,
-        theme: 'light' | 'dark',
-        modelValue: string,
-        id: string,
-        invalid?: {
-            status: boolean,
-            message: string
-        }
-    }
+type Props = {
+    label: string;
+    theme: "light" | "dark";
+    modelValue: string;
+    id: string;
+    validator?: any;
+};
 
 // props
 
-    defineProps<Props>()
-
+defineProps<Props>();
 </script>
 
 <template>
     <div class="flex flex-col gap-1.5 w-full">
-        <label 
-            :for="id" 
+        <label
+            :for="id"
             class="pr-1.5"
             :class="{
-                'text-white' : theme == 'dark',
-                'text-gray-950' : theme == 'light'
+                'text-white': theme == 'dark',
+                'text-gray-950': theme == 'light',
             }"
         >
             {{ label }}
         </label>
-        <input 
-            @change="$emit('update:modelValue', $event.target.value)"
-            :id="id" 
+        <input
+            @input="$emit('update:modelValue', $event.target.value)"
+            :id="id"
             class="w-full h-12 px-4 transition-all border outline-none focus:border-primary focus:ring-2 focus:ring-offset-2 focus:ring-primary rounded-100"
             :class="{
-                'ring-offset-[#19191A] text-white bg-[#19191A] border-gray-600 focus:bg-[#25272A]' : theme == 'dark',
-                'ring-offset-white text-black bg-white border-gray-300 focus:bg-[#f6f8fa]' : theme == 'light'
-            }"    
-        >
+                '!border-red-700 !ring-red-700': validator?.$error,
+                'ring-offset-[#19191A] text-white bg-[#19191A] border-gray-600 focus:bg-[#25272A]':
+                    theme == 'dark',
+                'ring-offset-white text-black bg-white border-gray-300 focus:bg-[#f6f8fa]':
+                    theme == 'light',
+            }"
+        />
+        <span v-if="validator?.$error" class="text-sm text-red-700 pr-1.5">
+            {{ validator?.$errors[0].$message }}
+        </span>
     </div>
 </template>
-
-<style scoped>
-
-</style>
