4db1e67fdc62005134f07d34207f252e4f9247c9
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-10 06:50:30 +0330

    updated useGetProduct

--- composables/api/product/useGetProduct.ts
+++ composables/api/product/useGetProduct.ts
@@ -6,30 +6,51 @@ import { API_ENDPOINTS, QUERY_KEYS } from "~/constants";
 
 // types
 
-type GetProductsResponse = Product
+export type GetProductResponse = {
+    id: number;
+    name: string;
+    slug: string;
+    description: string;
+    category: {
+        id: number;
+        name: string;
+        breadcrumb: string[];
+    };
+    thumbnail: string;
+    features_list: string[];
+    capability_list: string[];
+    categories: {
+        id: number;
+        name: string;
+        product_list: {
+            id: number;
+            name: string;
+            product_icon_photo: string;
+            slug: string;
+        }[];
+    }[];
+};
 
 type GetProductRequest = {
-    slug: string
-}
+    slug: string;
+};
 
 // methods
 
 const handleGetProduct = async ({ slug }: GetProductRequest) => {
-    const { data } = await axios.get<GetProductsResponse>(API_ENDPOINTS.product.getOne, {
-        params: {
-            slug
-        }
-    })
-    return data
-}
+    const { data } = await axios.get<GetProductResponse>(
+        `${API_ENDPOINTS.product.get}/${slug}`
+    );
+    return data;
+};
 
 // composable
 
-const useGetProduct = () => {
+const useGetProduct = (prdocutSlug: ComputedRef<string>) => {
     return useQuery({
-        queryKey: [QUERY_KEYS.product],
-        queryFn: ({ pageParam }) => handleGetProduct(pageParam)
-    })
-}
+        queryKey: [QUERY_KEYS.product, prdocutSlug],
+        queryFn: () => handleGetProduct({ slug: prdocutSlug.value }),
+    });
+};
 
-export default useGetProduct
+export default useGetProduct;
