4c99a7d5f669d3445cbe489688c2e5973ca4e4e8
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-12 03:32:37 +0330

    connected about us

--- components/home/AboutUs.vue
+++ components/home/AboutUs.vue
@@ -1,62 +1,74 @@
 <script setup lang="ts">
-
+// imports
 
 import useGetContent from "~/composables/api/useGetContent";
 
+// state
+
 const config = useRuntimeConfig();
+
+// queries
 
 const { data: content } = useGetContent();
 
-const imageUrl = computed(() => {
-    return config.public.API_BASE_URL + content.value!.about_us.image
-})
+// computed
 
+const imageUrl = computed(() => {
+    return config.public.API_BASE_URL + content.value!.about_us.image;
+});
 </script>
 
 <template>
-    <section class="w-full container flex-center">
-
-        <div class="w-full xl:w-[90%] flex items-center gap-12 max-lg:flex-col-reverse">
-
-            <div class="max-lg:hidden w-1/2">
-
-                <img :src="imageUrl" class="rounded-150 w-full" alt="about-us">
-
+    <section id="about-us-section" class="container w-full flex-center">
+        <div
+            class="w-full xl:w-[90%] flex items-center gap-12 max-lg:flex-col-reverse"
+        >
+            <div class="w-1/2 max-lg:hidden h-[30rem] overflow-hidden">
+                <NuxtImg
+                    :src="imageUrl"
+                    class="object-cover size-full rounded-150"
+                    alt="about-us"
+                />
             </div>
 
-            <div class="flex flex-col w-full lg:w-1/2 gap-6">
-
-                <span class="font-semibold text-primary">
+            <div
+                class="flex flex-col w-full max-lg:items-center max-lg:gap-3 lg:gap-6 lg:w-1/2"
+            >
+                <span class="font-semibold max-lg:text-center text-primary">
                     ما را بیشتر بشناسید
                 </span>
-                <span class="text-2xl font-semibold text-black">
-                    {{content!.about_us.title}}
+                <span
+                    class="text-2xl font-semibold text-black max-lg:text-center"
+                >
+                    {{ content!.about_us.title }}
                 </span>
-                <p class="leading-loose text-gray-800 text-justify">
-                    {{content!.about_us.description}}
+                <p
+                    class="leading-loose text-justify text-gray-800 max-lg:text-center"
+                >
+                    {{ content!.about_us.description }}
                 </p>
 
-                <div class="grid w-2/3 grid-cols-2 gap-10 persian-number">
-
-                    <div v-for="item in content!.about_us.projects" class="flex flex-col gap-1">
-                        <span class="text-4xl lg:text-5xl font-semibold text-primary">
+                <div
+                    class="grid w-2/3 grid-cols-2 gap-10 max-lg:mt-5 persian-number"
+                >
+                    <div
+                        v-for="(item, index) in content!.about_us.projects"
+                        :key="index"
+                        class="flex flex-col gap-1 max-lg:items-center"
+                    >
+                        <span
+                            class="text-4xl font-semibold lg:text-5xl text-primary"
+                        >
                             {{ item.count }}+
                         </span>
                         <span class="text-sm font-bold text-black">
                             {{ item.title }}
                         </span>
                     </div>
-
                 </div>
-
             </div>
-            
         </div>
-
-
     </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
