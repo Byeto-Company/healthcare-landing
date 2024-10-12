aec0ae146a4b85b304d86542c9e52a7f9de47ed8
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-12 03:30:52 +0330

    responsive footer

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -1,116 +1,139 @@
 <script setup lang="ts">
+// imports
 
 import useGetContent from "~/composables/api/useGetContent";
 
-const { data: content } = useGetContent();
+// state
 
+const { $vScrollTo: vScrollTo } = useNuxtApp();
+
+// queries
+
+const { data: content } = useGetContent();
 </script>
 
 <template>
-
-    <div class="z-higher w-full mt-24 shadow-sm bg-pattern-hex bg-gray-950">
-        <div class="container w-full h-full gap-12 flex-col-center padding-main">
-
+    <div class="w-full mt-24 shadow-sm z-higher bg-pattern-hex bg-gray-950">
+        <div
+            class="container w-full h-full gap-12 flex-col-center padding-main"
+        >
             <div
-                class="max-md:flex-col bg-primary w-full -mt-24 px-6 md:px-[3.5rem] py-10 md:py-[3rem] flex items-center justify-between [background-image:_url('/pattern-curve.png')] [background-size:_80%] rounded-250">
-
-                <div class="flex flex-col w-full md:w-1/2 items-start gap-[1.7rem] max-md:items-center">
-
-                    <span class="text-3xl md:text-4xl font-semibold text-white whitespace-nowrap max-md:text-center">
+                class="max-md:flex-col bg-primary w-full -mt-24 px-6 md:px-[3.5rem] py-10 md:py-[3rem] flex items-center justify-between [background-image:_url('/pattern-curve.png')] [background-size:_80%] rounded-250"
+            >
+                <div
+                    class="flex flex-col w-full md:w-1/2 items-start gap-[1.7rem] max-md:items-center"
+                >
+                    <span
+                        class="text-3xl font-semibold text-white md:text-4xl whitespace-nowrap max-md:text-center"
+                    >
                         {{ content?.footer.title }}
                     </span>
 
-                    <p class="text-white leading-[2rem] text-justify max-md:text-center">
+                    <p
+                        class="text-white leading-[2rem] text-justify max-md:text-center"
+                    >
                         {{ content?.footer.description }}
                     </p>
-
                 </div>
 
                 <div
                     class="flex max-md:mt-8 md:flex-col items-end gap-[1rem] max-sm:items-center max-md:justify-center max-md:flex-wrap"
                 >
-
-                    <button class="py-2.5 text-white px-7 bg-white/20 rounded-360 whitespace-nowrap">
-                        درخواست همکاری
+                    <button
+                        @click="
+                            vScrollTo.scrollTo('#demo-section', {
+                                offset: -120,
+                            })
+                        "
+                        class="py-2.5 text-white px-7 bg-white/20 rounded-360 whitespace-nowrap"
+                    >
+                        درخواست دمو
                     </button>
 
-                    <button class="py-2.5 bg-white px-7 text-primary rounded-360 whitespace-nowrap">
-                        ارتباط با ما
+                    <button
+                        @click="
+                            vScrollTo.scrollTo('#about-us-section', {
+                                offset: -120,
+                            })
+                        "
+                        class="py-2.5 bg-white px-7 text-primary rounded-360 whitespace-nowrap"
+                    >
+                        درباره ما
                     </button>
-
                 </div>
-
             </div>
 
-            <div class="w-full pb-12 border-b border-gray-800 flex-center max-md:flex-col max-md:gap-16 max-md:mt-8">
-
-                <div class="flex flex-col gap-[1.7rem] w-full md:w-1/2 max-md:items-center">
-
-                    <img src="/logo-row.png" class="w-[10rem]" alt="logo">
+            <div
+                class="w-full pb-12 border-b border-gray-800 flex-center max-md:flex-col max-md:gap-16 max-md:mt-8"
+            >
+                <div
+                    class="flex flex-col gap-[1.7rem] w-full md:w-1/2 max-md:items-center"
+                >
+                    <img src="/logo-row.png" class="w-[10rem]" alt="logo" />
                     <div class="flex flex-col gap-2">
                         <p class="text-sm text-gray-200 max-md:text-center">
                             شرکت فنی مهندسی خورشید رایانه طلوع
                         </p>
                         <p class="text-sm text-gray-200">
-                            {{content?.footer.address}}
+                            {{ content?.footer.address }}
                         </p>
                     </div>
                     <div class="flex items-center gap-8 max-md:justify-center">
-
-                        <NuxtLink class="text-gray-200">
-                            درباره ما
-                        </NuxtLink>
-                        <NuxtLink class="text-gray-200">
-                            حوزه کاری
-                        </NuxtLink>
-                        <NuxtLink class="text-gray-200">
-                            محصولات
-                        </NuxtLink>
-                        <NuxtLink class="text-gray-200">
-                            ویژگی ها
-                        </NuxtLink>
-
+                        <NuxtLink class="text-gray-200"> درباره ما </NuxtLink>
+                        <NuxtLink class="text-gray-200"> حوزه کاری </NuxtLink>
+                        <NuxtLink class="text-gray-200"> محصولات </NuxtLink>
+                        <NuxtLink class="text-gray-200"> ویژگی ها </NuxtLink>
                     </div>
-
                 </div>
 
                 <div
-                    class="flex items-start justify-end w-full md:w-1/2 gap-8 lg:gap-16 max-md:flex-col max-md:items-center">
-
-                    <div class="flex flex-col items-start gap-[1rem] max-md:items-center">
+                    class="flex items-start justify-end w-full gap-8 md:w-1/2 lg:gap-16 max-md:flex-col max-md:items-center"
+                >
+                    <div
+                        class="flex flex-col items-start gap-[1rem] max-md:items-center"
+                    >
                         <span class="text-primary">ایمیل</span>
-                        <span class="text-gray-300" v-for="email in content?.footer.emails">
-                            {{email}}
+                        <span
+                            class="text-gray-300"
+                            v-for="email in content?.footer.emails"
+                        >
+                            {{ email }}
                         </span>
                     </div>
 
-                    <div class="flex flex-col items-start gap-[1rem]  max-md:items-center">
+                    <div
+                        class="flex flex-col items-start gap-[1rem] max-md:items-center"
+                    >
                         <span class="text-primary">تلفن های تماس</span>
-                        <span style="direction: ltr !important;" class="text-gray-300" v-for="phone in content?.footer.phones">
-                            {{phone}}
+                        <span
+                            style="direction: ltr !important"
+                            class="text-gray-300"
+                            v-for="phone in content?.footer.phones"
+                        >
+                            {{ phone }}
                         </span>
                     </div>
-
                 </div>
-
             </div>
 
-            <div class="flex items-center justify-between w-full pb-6 -mt-6 max-sm:flex-col max-md:gap-6">
-                <p class="text-gray-500">@2024 شرکت فنی مهندسی خورشید رایانه طلوع</p>
+            <div
+                class="flex items-center justify-between w-full pb-6 -mt-6 max-sm:flex-col max-md:gap-6"
+            >
+                <p class="text-gray-500">
+                    @2024 شرکت فنی مهندسی خورشید رایانه طلوع
+                </p>
 
                 <div class="flex items-center gap-7">
                     <i class="text-2xl text-gray-500 fa-brands fa-whatsapp"></i>
-                    <i class="text-2xl text-gray-500 fa-brands fa-instagram"></i>
+                    <i
+                        class="text-2xl text-gray-500 fa-brands fa-instagram"
+                    ></i>
                     <i class="text-2xl text-gray-500 fa-brands fa-telegram"></i>
                     <i class="text-2xl text-gray-500 fa-brands fa-twitter"></i>
                 </div>
             </div>
-
         </div>
     </div>
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
