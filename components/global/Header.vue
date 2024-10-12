d4b046f670b7fb2c5004f04d55107d32e3ec3ddc
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-12 03:31:41 +0330

    connected nav links

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -1,68 +1,107 @@
 <script setup lang="ts">
+import useGetContent from "~/composables/api/useGetContent";
 
 // types
 
 interface navLink {
-    title: string,
-    path: string
+    title: string;
+    path: string;
 }
 
 // state
 
+const config = useRuntimeConfig();
 const route = useRoute();
+const router = useRouter();
+const { $vScrollTo: vScrollTo } = useNuxtApp();
 
 const isDrawerShow = ref<boolean>(false);
 
 const navLinks = ref<navLink[]>([
     {
-        title: "درباره ما",
-        path: "#about-us"
-    },
-    {
-        title: "حوزه کاری",
-        path: "#fields"
+        title: "حوزه های کاری",
+        path: "#work-fields-section",
     },
     {
         title: "محصولات",
-        path: "#products"
+        path: "#products-section",
     },
     {
-        title: "ویژگی ها",
-        path: "#features"
+        title: "مدیران",
+        path: "#leaders-section",
     },
     {
         title: "تایید نامه ها",
-        path: "#degrees"
+        path: "#confirmations-section",
     },
     {
-        title: "اعضا",
-        path: "#team"
+        title: "دمو",
+        path: "#demo-section",
     },
     {
-        title: "ارتباط با ما",
-        path: "#contact-us"
-    }
+        title: "درباره ما",
+        path: "#about-us-section",
+    },
 ]);
 
-// computed
+// queries
 
-const currentHash = computed(() => route.hash);
+const { data: content } = useGetContent();
 
+const logo = computed(() => {
+    if (content.value) {
+        return {
+            link: `${config.public.API_BASE_URL}/${content.value.logo.link}`,
+            alt: content.value.body_logo.link,
+        };
+    } else {
+        return {
+            link: "",
+            alt: "",
+        };
+    }
+});
+
+// methods
+
+const goToSection = (link: { title: string; path: string }) => {
+    isDrawerShow.value = false;
+    router.push({ hash: link.path });
+    if (route.path !== "/") {
+        navigateTo("/");
+        setTimeout(() => {
+            vScrollTo.scrollTo(`${link.path}`, { offset: -120 });
+        }, 250);
+    } else {
+        vScrollTo.scrollTo(`${link.path}`, { offset: -120 });
+    }
+};
+
+// life-cycle
+
+onMounted(() => {
+    if (!!route.hash) {
+        vScrollTo.scrollTo(`${route.hash}`, { offset: -120 });
+    }
+});
 </script>
 
 <template>
-    <div class="sticky top-0 z-50 w-full shadow-sm bg-white">
-        <div class="flex container justify-between items-center h-[4.75rem] z-higher padding-main">
-
-            <div class="flex items-center justify-start w-11/12 gap-[3rem] max-lg:hidden">
+    <div class="sticky top-0 z-50 w-full bg-white shadow-sm">
+        <div
+            class="flex container justify-between items-center h-[4.75rem] z-higher padding-main"
+        >
+            <div
+                class="flex items-center justify-start w-11/12 gap-[3rem] max-lg:hidden"
+            >
                 <NuxtLink
                     v-for="(link, index) in navLinks"
-                    :to="link.path"
                     :key="index"
                     :class="{
-                        'text-primary font-semibold' : currentHash == link.path
+                        'text-primary font-semibold': route.hash == link.path,
                     }"
-                    class="transition-all whitespace-nowrap"
+                    class="transition-all cursor-pointer whitespace-nowrap"
+                    @click="goToSection(link)"
                 >
                     {{ link.title }}
                 </NuxtLink>
@@ -73,20 +112,17 @@ const currentHash = computed(() => route.hash);
             </div>
 
             <NuxtLink to="/" class="w-[90px] lg:w-1/12 flex-center">
-                <img src="/logo-row.png" class="object-fill" alt="logo">
+                <img :src="logo.link" :alt="logo.alt" class="object-fill" />
             </NuxtLink>
-
         </div>
         <ClientOnly>
             <Drawer
                 v-model="isDrawerShow"
+                @goToSection="goToSection"
                 :navLinks="navLinks"
             />
         </ClientOnly>
-
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
