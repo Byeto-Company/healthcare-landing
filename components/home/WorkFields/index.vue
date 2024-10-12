6541ce38b1d1a84cc7beaf397d07da14dc5d6796
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-12 03:30:24 +0330

    changed work field

---
+++ components/home/WorkFields/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+// imports
+
+import useGetContent from "~/composables/api/useGetContent";
+
+// state
+
+const { data: content } = useGetContent();
+</script>
+
+<template>
+    <section id="work-fields-section">
+        <SectionTitle
+            :title="content?.work_fields.title"
+            :description="content?.work_fields.description"
+        />
+        <div
+            class="container flex justify-center gap-12 mt-20 padding-main max-md:flex-wrap md:gap-8 lg:gap-16 md:mt-28"
+        >
+            <WorkCard
+                v-for="(field, index) in content?.work_fields.fields"
+                :key="index"
+                :title="field.title"
+                :image="field.image.image"
+                :alt="field.image.alt"
+                :description="field.description"
+                :tags="field.tags"
+                :active="index == 1"
+            />
+        </div>
+    </section>
+</template>
+
+<style scoped></style>
