cdff2c88a788e394a135eab64805e0e901bfbc31
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-12 03:31:53 +0330

    responsived section title

--- components/global/SectionTitle.vue
+++ components/global/SectionTitle.vue
@@ -1,30 +1,32 @@
 <script setup lang="ts">
-
 // props
 
 type SectionTitleProps = {
     title: string;
     description?: string;
-    theme?: 'light' | 'dark';
-}
+    theme?: "light" | "dark";
+};
 
 withDefaults(defineProps<SectionTitleProps>(), {
-    theme: 'light'
-})
-
+    theme: "light",
+});
 </script>
 
 <template>
     <div class="flex flex-col items-center justify-center gap-4">
-        <h2 class="text-2xl lg:text-3xl font-semibold text-center" :class="theme == 'light' ? 'text-black' : 'text-white'">
+        <h2
+            class="text-2xl font-semibold text-center lg:text-3xl"
+            :class="theme == 'light' ? 'text-black' : 'text-white'"
+        >
             {{ title }}
         </h2>
-        <p class="lg:text-lg mt-2 text-center max-w-[700px] px-4" :class="theme == 'light' ? 'text-gray-800' : 'text-gray-200'">
+        <p
+            class="lg:text-lg lg:mt-2 text-center max-w-[700px] px-4"
+            :class="theme == 'light' ? 'text-gray-800' : 'text-gray-200'"
+        >
             {{ description }}
         </p>
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
