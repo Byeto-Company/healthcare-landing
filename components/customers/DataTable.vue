417b1e1ce07168818e92718b8723939ee7c61f94
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-14 22:17:40 +0330

    fixed data table data in customers

--- components/customers/DataTable.vue
+++ components/customers/DataTable.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 // imports
 
 import useGetCustomers from "~/composables/api/customers/useGetCustomers";
@@ -9,43 +8,35 @@ import useGetCustomers from "~/composables/api/customers/useGetCustomers";
 const { data: customers } = useGetCustomers();
 
 const customersList = computed(() => {
-    return customers.value?.pages.map(i => i.results).flat();
+    return customers.value?.pages.map((i) => i.customers.results).flat();
 });
-
 </script>
 
 <template>
-    <tbody
-        class="[&>*:nth-child(odd)]:bg-[#00000005]"
-    >
-    <tr
-        v-for="(customer, index) in customersList"
-        :key="index"
-    >
-        <th class="w-1/4 py-0.5 ps-8">
-            <div class="flex py-5 font-medium text-black/50">
-                {{ customer.name }}
-            </div>
-        </th>
-        <th class="w-1/4 py-0.5 px-6">
-            <div class="flex py-5 font-medium text-black/50">
-                {{ customer.corporate_date }}
-            </div>
-        </th>
-        <th class="w-1/4 py-0.5 px-6">
-            <div class="flex py-5 font-medium text-black/50">
-                {{ customer.name }}
-            </div>
-        </th>
-        <th class="w-1/4 py-0.5 px-6">
-            <div class="flex justify-center py-5 font-medium text-black/50">
-                {{ customer.city }}
-            </div>
-        </th>
-    </tr>
+    <tbody class="[&>*:nth-child(odd)]:bg-[#00000005]">
+        <tr v-for="(customer, index) in customersList" :key="index">
+            <th class="w-1/4 py-0.5 ps-8">
+                <div class="flex py-5 font-medium text-black/50">
+                    {{ customer.name }}
+                </div>
+            </th>
+            <th class="w-1/4 py-0.5 px-6">
+                <div class="flex py-5 font-medium text-black/50">
+                    {{ customer.corporate_date }}
+                </div>
+            </th>
+            <th class="w-1/4 py-0.5 px-6">
+                <div class="flex py-5 font-medium text-black/50">
+                    {{ customer.name }}
+                </div>
+            </th>
+            <th class="w-1/4 py-0.5 px-6">
+                <div class="flex justify-center py-5 font-medium text-black/50">
+                    {{ customer.city }}
+                </div>
+            </th>
+        </tr>
     </tbody>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
