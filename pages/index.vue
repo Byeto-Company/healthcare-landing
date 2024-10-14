bd384b9abd54bd0e641068c6c5732a55f5224112
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-10-14 22:15:55 +0330

    added meta in main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+// imports
+
+import useGetContent from "~/composables/api/useGetContent";
+
+// queries
+
+const { data: content } = useGetContent();
+
+// meta
+
+useHead({
+    title: content.value?.meta.title,
+    meta: [
+        { name: "description", content: content.value?.meta.description },
+        { name: "keywords", content: content.value?.meta.keywords },
+    ],
+});
+</script>
+
 <template>
     <div class="w-full h-full flex flex-col items-center gap-[6.25rem]">
         <Hero />
