3adb6384c37c6356452254788fb739d5e3ab7217
marzban-dev <marzban.dev@gmail.com>
2024-10-15 16:08:29 +0330

    Fix ssr cache

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,23 +4,38 @@ export default defineNuxtConfig({
 
     ssr: true,
 
+    routeRules: {
+        "/": {
+            swr: 3600
+        },
+        "/contact-us": {
+            swr: 3600
+        },
+        "/customers": {
+            swr: 3600
+        },
+        "/product/**": {
+            swr: 3600
+        }
+    },
+
     app: {
         head: {
             link: [
                 {
                     rel: "stylesheet",
                     type: "text/css",
-                    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css",
-                },
-            ],
+                    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+                }
+            ]
         },
         pageTransition: {
             enterActiveClass:
                 "animate__animated animate__fadeIn animate__faster",
             leaveActiveClass:
                 "animate__animated animate__fadeOut animate__faster",
-            mode: "out-in",
-        },
+            mode: "out-in"
+        }
     },
 
     css: [
@@ -28,33 +43,33 @@ export default defineNuxtConfig({
         "~/assets/css/tailwind.css",
         "~/assets/css/style.css",
         "vue-toastification/dist/index.css",
-        "animate.css/animate.min.css",
+        "animate.css/animate.min.css"
     ],
 
     components: [
         {
             path: "~/components",
-            pathPrefix: false,
-        },
+            pathPrefix: false
+        }
     ],
 
     postcss: {
         plugins: {
             tailwindcss: {},
-            autoprefixer: {},
-        },
+            autoprefixer: {}
+        }
     },
 
     runtimeConfig: {
         public: {
             API_BASE_URL:
-                process.env.API_BASE_URL ?? "https://hcareapi.liara.run",
-        },
+                process.env.API_BASE_URL ?? "https://hcareapi.liara.run"
+        }
     },
 
     compatibilityDate: "2024-08-24",
 
     plugins: ["~/plugins/tanstack.ts"],
 
-    modules: ["@nuxt/image"],
+    modules: ["@nuxt/image"]
 });
